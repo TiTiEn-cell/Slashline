f217c796b7bfac35be98ac6a44db972475d812c6
TiTiEn-cell <ntantai123321@gmail.com>
2026-06-23 12:55:18 +0700

    Player move input

---
+++ Assets/_Project/_Scripts/Player/CharacterAuthoring.cs
@@ -0,0 +1,60 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using Unity.Physics;
+using Unity.Burst;
+
+public struct InitializeCharacterFlag: IComponentData, IEnableableComponent  { }
+public struct CharacterMoveDirection : IComponentData
+{
+    public float2 Value;
+}
+public struct CharacterMoveSpeed : IComponentData
+{
+    public float Value;
+}
+public class CharacterAuthoring : MonoBehaviour
+{
+    public float MoveSpeed;
+    private class Baker : Baker<CharacterAuthoring>
+    {
+        public override void Bake(CharacterAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<InitializeCharacterFlag>(entity);
+            AddComponent<CharacterMoveDirection>(entity);
+            AddComponent(entity, new CharacterMoveSpeed
+            {
+                Value = authoring.MoveSpeed
+            });
+        }
+    }
+}
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public partial struct CharacterInitializationSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (mass, shouldInitialize) in SystemAPI.Query<RefRW<PhysicsMass>, EnabledRefRW<InitializeCharacterFlag>>())
+        {
+            mass.ValueRW.InverseInertia = float3.zero;
+            shouldInitialize.ValueRW = false;
+        }
+    }
+}
+
+public partial struct CharacterMoveSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        //var deltaTime = SystemAPI.Time.DeltaTime;
+        foreach (var (velocity, direction, speed) in SystemAPI.Query<RefRW<PhysicsVelocity>, CharacterMoveDirection, CharacterMoveSpeed>())
+        {
+            var moveStep2d = direction.Value * speed.Value;
+            velocity.ValueRW.Linear = new float3(moveStep2d, 0f);
+        }
+    }
+}
