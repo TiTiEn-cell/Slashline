28168018baa30ad1d1153ce6db093338a013f52e
TiTiEn-cell <ntantai123321@gmail.com>
2026-06-25 09:13:27 +0700

    Temp file script

---
+++ Assets/_Project/_Scripts/Temp/PlayerAuthoring.cs
@@ -0,0 +1,89 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct PlayerTag : IComponentData { }
+public struct CameraTarget : IComponentData
+{
+    public UnityObjectRef<Transform> CameraTransform;
+}
+
+public struct InitializeCameraTargetTag : IComponentData { }
+
+[MaterialProperty("_AnimationIndex")]
+public struct AnimationIndexOverride : IComponentData
+{
+    public float Value;
+}
+
+public class PlayerAuthoring : MonoBehaviour
+{
+    private class Baker : Baker<PlayerAuthoring>
+    {
+        public override void Bake(PlayerAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<PlayerTag>(entity);
+            AddComponent<InitializeCameraTargetTag>(entity);
+            AddComponent<CameraTarget>(entity);
+            AddComponent<AnimationIndexOverride>(entity);
+        }
+    }
+}
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public partial struct CameraInitializationSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<InitializeCameraTargetTag>();
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        if (CameraTargetSingleton.Instance == null) return;
+        var cameraTargetTransform = CameraTargetSingleton.Instance.transform;
+
+        var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
+        foreach (var (cameraTarget, entity) in SystemAPI.Query<RefRW<CameraTarget>>().WithAll<InitializeCameraTargetTag, PlayerTag>().WithEntityAccess())
+        {
+            cameraTarget.ValueRW.CameraTransform = cameraTargetTransform;
+            ecb.RemoveComponent<InitializeCameraTargetTag>(entity);
+        }
+
+        ecb.Playback(state.EntityManager);
+    }
+}
+
+[UpdateAfter(typeof(TransformSystemGroup))]
+public partial struct MoveCameraSystem : ISystem
+{
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (transform, cameraTarget) in SystemAPI.Query<LocalToWorld, CameraTarget>()
+            .WithAll<PlayerTag>()
+            .WithNone<InitializeCameraTargetTag>())
+        {
+            cameraTarget.CameraTransform.Value.transform.position = transform.Position;
+        }
+    }
+}
+public partial class PlayerInputSystem : SystemBase
+{
+    private PlayerInput _input;
+    protected override void OnCreate()
+    {
+        _input = new PlayerInput();
+        _input.Enable();
+    }
+    protected override void OnUpdate()
+    {
+        var currentInput = (float2)_input.Player.Move.ReadValue<Vector2>();
+        foreach (var direction in SystemAPI.Query<RefRW<CharacterMoveDirection>>().WithAll<PlayerTag>())
+        {
+            direction.ValueRW.Value = currentInput;
+        }
+    }
+}
